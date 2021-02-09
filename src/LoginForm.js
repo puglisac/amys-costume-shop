1da63d2684148904ad8bc238544d8be2e255f524
puglisac <alan.c.puglisi@gmail.com>
2021-02-09 17:01:11 -0600

    login form functionality

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,17 +1,36 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { InputGroup, InputGroupAddon, Button, Input, Form } from 'reactstrap';
 
 const LoginForm = (props) => {
+    const initialState = {
+        email: "",
+        password: "",
+    };
+    const [formData, setFormData] = useState(initialState);
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((data) => ({
+            ...data,
+            [name]: value
+        }));
+    };
+
     return (
         <div className="container mt-4 col-md-4">
             <Form>
                 <InputGroup>
-                    <Input placeholder="email" />
+                    <Input placeholder="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange} />
                 </InputGroup>
                 <br />
                 <InputGroup>
-                    <Input placeholder="password" />
+                    <Input placeholder="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange} />
                     <InputGroupAddon addonType="append">
                         <Button color="primary"> Login </Button>
                     </InputGroupAddon>
