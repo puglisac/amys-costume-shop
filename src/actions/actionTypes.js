5bcbc43b8533d112b19d5d5ce2ae239d53c51ccb
Alan Puglisi <alan.c.puglisi@gmail.com>
2021-02-16 11:09:49 -0600

    updated action types

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,10 +1,18 @@
+// login actionTypes
 export const GET_TOKEN = "GET_TOKEN";
 export const LOGOUT = "LOGOUT";
 export const GET_CURR_USER = "GET_USER";
+// items actionTypes
 export const GET_ITEMS = "GET_ITEMS";
 export const ADD_ITEM = "ADD_ITEM";
+export const REMOVE_ITEM = "REMOVE_ITEM";
+// categories actionTypes
 export const GET_CATEGORIES = "GET_CATEGORIES";
 export const ADD_CATEGORY = "ADD_CATEGORY";
+export const REMOVE_CATEGORY = "REMOVE_CATEGORY";
+// users actionTypes
 export const GET_USERS = "GET_USERS";
 export const ADD_USER = "ADD_USER";
+export const REMOVE_USER = "REMOVE_USER";
+// api url
 export const INVENTORY_URL = "http://127.0.0.1:5000/";
