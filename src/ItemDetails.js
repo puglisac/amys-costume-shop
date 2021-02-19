33b7e9c3ee5612e2716576117f4a932fc5f55a45
Alan Puglisi <alan.c.puglisi@gmail.com>
2021-02-19 09:48:40 -0600

    items details

--- src/ItemDetails.js
+++ src/ItemDetails.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getOneItem } from './actions/items';
 import FormModal from './FormModal';
+import { CardImg, CardBody, CardTitle, CardSubtitle, CardText, Card } from 'reactstrap';
 
 const ItemDetails = () => {
     // shows details about an item
@@ -15,9 +16,17 @@ const ItemDetails = () => {
         dispatch(getOneItem(token, item_id)).catch(e => alert(e));
     }, []);
     return (
-        <div>
-            {items ? items.name : "Loading..."}
-            {currUser.is_admin ? <FormModal buttonLabel="Edit Item" formType="item" item={items} /> : null}
+        <div className="container">
+            {!Array.isArray(items) ? <Card>
+                <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+                <CardBody>
+                    <CardTitle tag="h5">{items.Name}</CardTitle>
+                    <CardSubtitle tag="h6" className="mb-2 text-muted">{items.location}</CardSubtitle>
+                    <CardText>{items.description}</CardText>
+                    <CardText>Quantity: {items.quantity}</CardText>
+                    {currUser.is_admin ? <FormModal buttonLabel="Edit Item" formType="item" item={items} /> : null}
+                </CardBody>
+            </Card> : "Loading..."}
         </div>
     );
 };
