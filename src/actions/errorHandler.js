00368b00f5b29b5ce02398aa55e7c113141bed7d
puglisac <alan.c.puglisi@gmail.com>
2021-03-22 18:39:55 -0500

    fixed error handler

--- src/actions/errorHandler.js
+++ src/actions/errorHandler.js
@@ -1,17 +1,19 @@
 import { LOGOUT } from "./actionTypes";
 
 export const errorHandler = (e, dispatch, cat) => {
-    if (e.response.status == 404) {
-        throw new Error(`No such ${cat}`);
-    } else if (e.response.status == 401) {
-        if (e.response.data.msg && e.response.data.msg == "Token has expired") {
-            alert("Please log back in");
-            dispatch({ type: LOGOUT });
-        }
-        else alert(e.response.data.message);
-    } else if (e.response) {
-        throw new Error(e.response.data.message);
-    } else {
+    if (e.response) {
+        if (e.response.status == 404) {
+            throw new Error(`No such ${cat}`);
+        } else if (e.response.status == 401) {
+            if (e.response.data.msg && e.response.data.msg == "Token has expired") {
+                alert("Please log back in");
+                dispatch({ type: LOGOUT });
+            }
+            else alert(e.response.data.message);
+        } else
+            throw new Error(e.response.data.message);
+    }
+    else {
         throw new Error(e);
     }
 };
